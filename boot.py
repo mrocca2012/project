41bd6adcc257dec119eb483f89ca68ab7d5200e6
mrocca2012 <mrocca2015@gmail.com>
2026-01-07 21:25:42 -0400

    Update boot.py

--- boot.py
+++ boot.py
@@ -9,7 +9,7 @@ file_version = 1.1
 freq(160000000)
 
 # --- Configuracion de OTA ---
-GITHUB_URL = "https://raw.githubusercontent.com/mrocca2012/project/master/"
+GITHUB_URL = "https://raw.githubusercontent.com/mrocca2012/esp32water/master/"
 
 def connect_to_wifi():
     """
